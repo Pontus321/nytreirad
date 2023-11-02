947b498706e873fe375fe45d841d0e65f7608a2b
Pontus321 <p_pontus@hotmail.com>
2023-11-02 12:52:44 +0100

    objektest

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,8 +1,102 @@
+import java.util.ArrayList;
+import java.util.List;
 
+class GameBoard {
+    private char[][] gameBoard;
+    private ArrayList<Integer> player1Positions;
+    private ArrayList<Integer> player2Positions;
 
-//Print out the gameboard
-public class GameBoard {
-    public static void printGameBoard(char[][] gameBoard) {
+    public GameBoard() {
+        gameBoard = new char[][] {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+
+        player1Positions = new ArrayList<>();
+        player2Positions = new ArrayList<>();
+    }
+
+    public void placePiece(int pos, String user) {
+        char symbol = ' ';
+        if (user.equals("Player 1")) {
+            symbol = 'X';
+            player1Positions.add(pos);
+        } else if (user.equals("Player 2")) {
+            symbol = 'O';
+            player2Positions.add(pos);
+        }
+
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+        }
+    }
+
+    public boolean isPositionTaken(int pos) {
+        return player1Positions.contains(pos) || player2Positions.contains(pos);
+    }
+
+    public String checkWinner() {
+        List<Integer> topRow = List.of(1, 2, 3);
+        List<Integer> midRow = List.of(4, 5, 6);
+        List<Integer> botRow = List.of(7, 8, 9);
+        List<Integer> leftCol = List.of(1, 4, 7);
+        List<Integer> midCol = List.of(2, 5, 8);
+        List<Integer> rightCol = List.of(3, 6, 9);
+        List<Integer> cross1 = List.of(1, 5, 9);
+        List<Integer> cross2 = List.of(7, 5, 3);
+
+        List<List<Integer> > winning = new ArrayList<>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for (List<Integer> l : winning) {
+            if (player1Positions.containsAll(l)) {
+                return "Player 1 wins!";
+            } else if (player2Positions.containsAll(l)) {
+                return "Player 2 wins!";
+            } else if (player1Positions.size() + player2Positions.size() == 9) {
+                return "Tie";
+            }
+        }
+        return "";
+    }
+
+    public void printGameBoard() {
         for (char[] row : gameBoard) {
             for (char c : row) {
                 System.out.print(c);
@@ -10,6 +104,12 @@ public class GameBoard {
             System.out.println();
         }
     }
+
+    public void resetBoard() {
+        gameBoard = new char[][] {
+        };
+
+        player1Positions.clear();
+        player2Positions.clear();
+    }
 }
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,15 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    // These two ArrayLists will keep track of the positions of Player 1 and Player 2 on the game board.
-    //player class gamelogic
-    static ArrayList<Integer> player1Positions = new ArrayList<>();
-    static ArrayList<Integer> player2Positions = new ArrayList<>();
-
     public static void main(String[] args) {
         while (true) {
-            // Create a 3x3 game board using a 2D character array.
-            char[][] gameBoard = {
-                    {' ', '|', ' ', '|', ' '},
-                    {'-', '+', '-', '+', '-'},
-                    {' ', '|', ' ', '|', ' '},
-                    {'-', '+', '-', '+', '-'},
-                    {' ', '|', ' ', '|', ' '}};
+            GameBoard gameBoard = new GameBoard();
 
             while (true) {
-                // Print the game board to the console.
-                GameBoard.printGameBoard(gameBoard);
+                gameBoard.printGameBoard();
+                playerTurn(gameBoard, "Player 1");
 
-                // Allow Player 1 to make their move.
-                Player.playerTurn(gameBoard, "Player 1", player1Positions, player2Positions);
-
-
-                // Check if anyone wins or if it's a tie.
-                String result = GameLogic.checkWinner(player1Positions, player2Positions);
+                String result = gameBoard.checkWinner();
                 if (result.equals("Player 1 wins!")) {
                     System.out.println("Player 1 wins!");
                     break;
@@ -38,12 +21,9 @@ public class Main {
                     break;
                 }
 
-                // Print the final state of the game board.
-                GameBoard.printGameBoard(gameBoard);
-
-                // Allow Player 2 to make their move.
-                Player.playerTurn(gameBoard, "Player 2", player1Positions, player2Positions);
-                result = GameLogic.checkWinner(player1Positions, player2Positions);
+                gameBoard.printGameBoard();
+                playerTurn(gameBoard, "Player 2");
+                result = gameBoard.checkWinner();
                 if (result.equals("Player 1 wins!")) {
                     System.out.println("Player 1 wins!");
                     break;
@@ -55,22 +35,25 @@ public class Main {
                     break;
                 }
             }
-
-            // Print the final state of the game board.
-            GameBoard.printGameBoard(gameBoard);
-
-            // Ask the players if they want to play another round.
+            gameBoard.printGameBoard();
             System.out.println("Do you want to play again? (Y/N)");
             Scanner scanner = new Scanner(System.in);
             String response = scanner.next();
             if (!response.equalsIgnoreCase("Y")) {
                 break;
             }
-
-            // Reset the player positions when the game is played again.
-            player1Positions.clear();
-            player2Positions.clear();
+            gameBoard.resetBoard();
         }
     }
 
+    public static void playerTurn(GameBoard gameBoard, String playerName) {
+        System.out.println(playerName + ", choose your position (1-9):");
+        Scanner scan = new Scanner(System.in);
+        int playerPos = scan.nextInt();
+        while (gameBoard.isPositionTaken(playerPos)) {
+            System.out.println("Position is taken, choose another one.");
+            playerPos = scan.nextInt();
+        }
+        gameBoard.placePiece(playerPos, playerName);
+    }
 }
